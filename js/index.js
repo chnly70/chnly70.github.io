5080ae77fa1e1beba0dd35c477a4a1bb4d8173b8
chnly <alice31035@gmail.com>
2019-05-08 22:05:48 +0800

    20190508上課內容：修改0424作業，建立js變數

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//警示視窗
+alert('Hello World!');
+//變數(不可放數字)
+var message = 'Hello World!';
+
+//8.9行意思相同
+msg = 'Hello World';
+msg += 'foobar';
+msg = msg + 'foobar';
+
+//變數相加
+var a = 'Hello World!';
+var b = 'foobar';
+var msg = a+b;
+
+//數值相加
+var a = 1;
+var b = 2;
+var msg = a+b;
+alert(msg);
+
+//引號是字串
+var a ='1';
+var b ='2';
+var c ='3';
+var msg = a + b + parseInt(c);
+alert(msg);
