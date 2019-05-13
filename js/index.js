2bccde60659ee5ec2935c5a468c2366d9714edf0
chnly <alice31035@gmail.com>
2019-05-13 14:50:02 +0800

    0508回家作業： - 1加到50 - (3+2-1)/(8+2-6-2)=a

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 //警示視窗
 alert('Hello World!');
 //變數(不可放數字)
@@ -24,4 +25,15 @@ var a ='1';
 var b ='2';
 var c ='3';
 var msg = a + b + parseInt(c);
-alert(msg);
+alert(msg);*/
+
+
+//20190508 作業-1
+var a = 1;
+var b = 50;
+var total = (a+b)*b/2;
+alert (total);
+
+//20190508 作業-2
+var a = (3+2-1)/(8+2-6-2);
+alert (a);
