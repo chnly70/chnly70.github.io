6aae695c9e4d2de17c5edbc5ca38a0709c51eb80
chnly <alice31035@gmail.com>
2019-06-03 13:19:32 +0800

    no message

--- js/0527_index.js
+++ js/0527_index.js
@@ -1,5 +1,6 @@
-/*
+
 //迴圈
+/*
 var total = 0 ;
 for (var i = 2; i<= 100; i++) {
 	if (i % 2 == 0){
@@ -10,7 +11,7 @@ console.log(total);
 
 
 //js改div
-var div $('<div/>>').css({
+var div $('<div/>').css({
 	'background': 'red',
 	'width': '50px',
 	'height': '50px'
@@ -21,21 +22,20 @@ console.log(div);
 $('#canvas').append(div);
 
 //陣列
-
-/*var a = [];
+var a = [];
 for (var i = 0; i<100; i++) {
 	a.push(i);
 }
 console.log(a);
+*/
 
 var colors = ['red','blue','green','yellow'];
-//console.log(colors[2]);
+//console.log(colors[0]);
 //console.log(colors.length);
 for (var i = 0 ; i < colors.length; i++) {
 	console.log(colors[i]);
 }
-*/
-
+/*
 //陣列+迴圈
 var colors = ['red','blue','green','yellow'];
 
@@ -49,3 +49,4 @@ for (var i = 0 ; i < colors.length; i++) {
 //DOM 操作
 $('#canvas').append(div);
 }
+*/
