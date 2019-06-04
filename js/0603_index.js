e0cdba9a5120dfe28326e5d911c57cd3049f66fe
chnly <alice31035@gmail.com>
2019-06-05 00:47:25 +0800

    20190605 - 回家作業 ( 不會寫 )

---
+++ js/0603_index.js
@@ -0,0 +1,21 @@
+//第一題
+var colors = ['red','yellow','green','blue'];
+for (var i = 0; i < colors.length; i++) {
+	var h1 = $('<h1/>').css{(
+		'color':'red';
+		)};
+}
+
+//第二題
+var div = $('.box') .css({
+	'width': '50px',
+	'height':'50px',
+	'border':'2px solid #000'
+
+});
+
+for (var i = 0 ; i < 200; i++) {
+	console.log('.box');
+};
+
+
