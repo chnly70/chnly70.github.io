635d8b9bc4ff872d719a3ce857c7916c6373ec0e
chnly <alice31035@gmail.com>
2019-06-07 00:53:17 +0800

    作業檢討

--- js/0603_index.js
+++ js/0603_index.js
@@ -1,21 +1,42 @@
 //第一題
-var colors = ['red','yellow','green','blue'];
-for (var i = 0; i < colors.length; i++) {
-	var h1 = $('<h1/>').css{(
-		'color':'red';
-		)};
-}
+$('#selecTest').on('change',function(){
+	var color = $('#selecTest').val();
 
-//第二題
-var div = $('.box') .css({
-	'width': '50px',
-	'height':'50px',
-	'border':'2px solid #000'
-
+	$('#text').css({
+		'color':color
+	});
 });
 
-for (var i = 0 ; i < 200; i++) {
-	console.log('.box');
-};
+//第二題
+/*for (var i =1 ; i <= 200; i++) {
+		var div = $('<div/>').css({
+		'width': '50px',
+		'height':'50px',
+		'border':'2px solid #000',
+		'margin':'10px',
+		'float':'left'
+		});
+		div.text(i);
+
+		$('#box').append(div);
+}*/
+
+for (var i = 1; i <= 200; i++) {
+	var n = (i % 4) + 1;
+	
+	var div = $('<div/>').addClass('myclass bg_'+ n);
+	div.text(i);
+	$('#box').append(div);
+}
+
+//第三題
+var j = 0;
+$('#btn_add').on('click',function(){
+	var div = $('<div/>').addClass('myclass');
+	div.text(j);
+	$('#box_2').append(div);
+	j++;
+
+});
 
 
