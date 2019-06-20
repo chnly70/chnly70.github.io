c1b0577bcf1280498d4198189f0cc6be9b1e112a
chnly <alice31035@gmail.com>
2019-06-20 13:30:08 +0800

    no message

--- js/HW_0619_index.js
+++ js/HW_0619_index.js
@@ -1,5 +1,22 @@
-$('#select_Number').on('change',function () {
-	var number = $('#select_Number') val();
-	$(.show_detail).append({i x 1++ = i*1++});
-	
+$('#select_Number').on('change', function() {
+    var i = $('#select_Number') val();
+    for (var i = 1; i <= 0; i) {
+    	for (var j =  1; j <= 0; j) {
+    		document.write(i "*"
+    		    j "="
+    		    i * j " ");
+    		if (i == j) { document.write("<br/>"); break; }
+    	}
+    }
+    $(.show_detail).append({ i x 1++ = i * 1++ });
+
 });
+
+/*for (i = 1; i <= 9; i) {
+    for (j = 1; j <= 9; j) {
+        document.write(i "*"
+            j "="
+            i * j " ");
+        if (i == j) { document.write("<br/>"); break; } //用的if語句，如果這兩個數字相同了 那麼跳出，另起一行
+    }
+}*/
