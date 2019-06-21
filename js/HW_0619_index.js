64af849de5f545fab7f43b30de839892a4d0f045
chnly <alice31035@gmail.com>
2019-06-22 02:21:47 +0800

    20190621 課堂作業檢討(九九乘法)+練習

--- js/HW_0619_index.js
+++ js/HW_0619_index.js
@@ -1,16 +1,27 @@
+/*var sum = 0;
 $('#select_Number').on('change', function() {
-    var i = $('#select_Number') val();
-    for (var i = 1; i <= 0; i) {
-    	for (var j =  1; j <= 0; j) {
-    		document.write(i "*"
-    		    j "="
-    		    i * j " ");
-    		if (i == j) { document.write("<br/>"); break; }
-    	}
-    }
-    $(.show_detail).append({ i x 1++ = i * 1++ });
+    $('.show_detail').html('');
+    var num = $('#select_Number').val();
+    for (var i = 1; i <= 9; i++) {
+        sum = num * i;
+        var sumstyle = $('<span>').text(num).addClass('gray');
+        $('.show_detail').append(sumstyle).append('*' + i + '=' + sum + '</br>');
 
-});
+    }
+
+});*/
+
+/*var text = $('<span>').text('Hello world.');
+$('.show_detail').append(text);*/
+
+var box1 = $('<div>').addClass('box red')
+$('.show_detail').append(box1);
+var box2 = $('<div>').addClass('box blue')
+$('.show_detail').append(box2);
+var box3 = $('<div>').addClass('box green')
+$('.show_detail').append(box3);
+
+
 
 /*for (i = 1; i <= 9; i) {
     for (j = 1; j <= 9; j) {
